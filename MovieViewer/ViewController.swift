aa290525dc949316f569cba1da55b54518bd8546
armendo <Fer@fers-macbook-pro.local>
2016-01-24 16:17:55 -0500

    Changes to test github

--- MovieViewer/ViewController.swift
+++ MovieViewer/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x  =    5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        x = 5 + 1
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
