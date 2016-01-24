24f66abe25308394e2191a49810b5e8767be79fe
armendo <Fer@fers-macbook-pro.local>
2016-01-24 16:05:31 -0500

    Initial Commit

---
+++ MovieViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewer
+//
+//  Created by Fer on 1/24/16.
+//  Copyright © 2016 Fernando Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
